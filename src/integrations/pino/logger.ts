43bfb82084fbb8cabaa06cb22d7f98f72c2b6c26
Josh Payette <hello@joshpayette.dev>
2026-04-20 13:07:59 -0400

    updated logger to be isomorphic

--- src/integrations/pino/logger.ts
+++ src/integrations/pino/logger.ts
@@ -1,16 +1,31 @@
+import { createIsomorphicFn } from "@tanstack/react-start";
 import pino from "pino";
 
-const logger = pino({
-	browser: {
-		asObject: true,
-	},
-	level: process.env.NODE_ENV === "production" ? "info" : "debug",
-	transport: {
-		target: "pino-pretty",
-		options: {
-			colorize: true,
-		},
-	},
-});
+const logger = createIsomorphicFn()
+	.server(() =>
+		pino({
+			level: process.env.NODE_ENV === "production" ? "info" : "debug",
+			transport: {
+				target: "pino-pretty",
+				options: {
+					colorize: true,
+				},
+			},
+		}),
+	)
+	.client(() =>
+		pino({
+			browser: {
+				asObject: true,
+			},
+			level: process.env.NODE_ENV === "production" ? "info" : "debug",
+			transport: {
+				target: "pino-pretty",
+				options: {
+					colorize: true,
+				},
+			},
+		}),
+	);
 
 export { logger };
