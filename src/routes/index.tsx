9fdd353c7f1755a83537513888c0af8ca5d773db
Josh Payette <hello@joshpayette.dev>
2026-04-26 20:34:57 -0400

    set gameid to none on home route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,15 @@
 import { Title } from "@mantine/core";
 import { createFileRoute } from "@tanstack/react-router";
+import { useEffect } from "react";
+import { setGame } from "#/features/game/store/game-store";
 
 export const Route = createFileRoute("/")({ component: App });
 
 function App() {
+	// set game to none for the home page
+	useEffect(() => {
+		setGame("none", "route");
+	}, []);
+
 	return <Title order={1}>Home Page</Title>;
 }
