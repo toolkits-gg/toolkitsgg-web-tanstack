59d39226246aa87eba4310d1db84c56fbf13d900
Josh Payette <hello@joshpayette.dev>
2026-05-04 11:13:27 -0400

    added rule exception

--- src/db.ts
+++ src/db.ts
@@ -1,12 +1,13 @@
 import { PrismaPg } from "@prisma/adapter-pg";
 import { serverEnv } from "#/config/env.js";
-import { PrismaClient } from "../prisma/generated/prisma/client.js";
+import { PrismaClient } from "@/prisma";
 
 const adapter = new PrismaPg({
 	connectionString: serverEnv.DATABASE_URL,
 });
 
 declare global {
+	// noinspection ES6ConvertVarToLetConst
 	var __prisma: PrismaClient | undefined;
 }
 
